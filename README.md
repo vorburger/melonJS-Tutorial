819b443cc6a0ee7df9e4adea14c2a6db0f35840e
Michael Vorburger <mike@vorburger.ch>
2013-10-13 00:47:38 +0200

    boilerplate + tutorial_data.zip AS-IS

---
+++ README.md
@@ -0,0 +1,6 @@
+melonJS boilerplate + tutorial data files + Vorburger's fixes
+
+read http://blog2.vorburger.ch/2013/10/melonjs.html
+
+try TBD
+
---
+++ js/screens/play.js
@@ -0,0 +1,23 @@
+game.PlayScreen = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+		// reset the score
+		game.data.score = 0;
+		
+		// add our HUD to the game world	
+		me.game.add(new game.HUD.Container());
+
+		me.levelDirector.loadLevel("area01");
+	},
+	
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		// remove the HUD from the game world
+		me.game.world.removeChild(me.game.world.getEntityByProp("name", "HUD")[0]);
+	}
+});
